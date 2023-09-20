0748d44587f0bfdaa1da7af51d5ac056ee9dcac5
Luke Wheldale <lukewheldale@gmail.com>
2023-09-20 11:49:21 +0930

    feat: submit button and ability to change name

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Personal from "./components/Personal";
+// import React from "react";
+
+// Section to add general information - name, edmail, phone number etc
+// Section to add education (school name, title of study, date of study)
+// Section to add practical experience (Company name, title of position, date of employment, description of duties)
+
+// Include edit and submit button for each seciton to allow for editing and saving of information
+// Submit button should submit form and display the value of input fields in HTML elements.
+// Edit button should add back (display) the input fields, with the previous displayed information as values in the input fields.
+// in the input fields, you should be able to edit and resubmit the content.
+// Make heavy use of state and props
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<h1>Resume</h1>
+			<section className="forms">
+				<Personal />
+			</section>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/Personal.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function Personal() {
+	const [name, setName] = useState("");
+	const [displayedName, setDisplayedName] = useState("");
+
+	const handleNameChange = (e) => {
+		setName(e.target.value);
+		setDisplayedName(e.target.value); // This is to display the name as it is being typed
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<div>
+			<h1>Personal Information</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={name}
+					required
+					onChange={handleNameChange}
+				/>
+				<button type="submit">Submit</button>
+			</form>
+			{displayedName && <h2>{displayedName}</h2>}
+		</div>
+	);
+}
+
+export default Personal;
